67d352e8c3f0878ea0e96a99a44b29720abea94e
turhanomer <omerturhan16@outlook.com>
2025-01-12 02:20:14 +0300

    Araç yönetimi kısmındaki araç ekleme formu düzenlendi

--- obj/Debug/net8.0/RentalCar.AssemblyInfo.cs
+++ obj/Debug/net8.0/RentalCar.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RentalCar")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a388a2d1f29d1dc324e5baf96598351cee90ac59")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38103e567dad648c2e1581895e97857627b575f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("RentalCar")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RentalCar")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
